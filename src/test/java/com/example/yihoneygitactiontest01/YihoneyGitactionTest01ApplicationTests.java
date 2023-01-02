6c7ce8fdfc692992d59d559192ce05c7a1a18418
yihoney <109622@naver.com>
2023-01-02 19:38:17 +0900

    Initial commit

---
+++ src/test/java/com/example/yihoneygitactiontest01/YihoneyGitactionTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yihoneygitactiontest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YihoneyGitactionTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
